730e324468622c46bf7649a134ca853cd88182e4
Milad Khoshdel <miladkhoshdel@gmail.com>
2026-05-08 13:21:51 +0330

    Add blog (posts) templates

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,9 +1,38 @@
+from django.views.generic import DetailView, ListView
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import BlogPost
 from .serializers import BlogPostSerializer
+
+
+class BlogPostListView(ListView):
+    """Public page that renders published weblog posts."""
+
+    model = BlogPost
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        return BlogPost.objects.select_related("author").filter(
+            status=BlogPost.Status.PUBLISHED
+        )
+
+
+class BlogPostDetailView(DetailView):
+    """Public page that renders a single published weblog post."""
+
+    model = BlogPost
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
+
+    def get_queryset(self):
+        return BlogPost.objects.select_related("author").filter(
+            status=BlogPost.Status.PUBLISHED
+        )
 
 
 class BlogPostViewSet(viewsets.ModelViewSet):
